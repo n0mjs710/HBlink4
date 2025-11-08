cd903f72786fbef0dd440df3a0d1e8b0b3a2f820
cort <cort@vesta.k0usy.org>
2025-11-08 14:33:20 -0600

    Add missing socket module import

--- hblink4/hblink.py
+++ hblink4/hblink.py
@@ -14,6 +14,7 @@ import logging
 import logging.handlers
 import pathlib
 import ipaddress
+import socket
 from typing import Dict, Any, Optional, Tuple, Union, List, Set
 from time import time
 from random import randint
